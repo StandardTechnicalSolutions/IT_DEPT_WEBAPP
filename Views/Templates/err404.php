43aba55fd4fd7de66cb0af45be29834c4f36f5d5
Jaleel Blue <jaleel.blue97@gmail.com>
2019-09-14 02:29:53 -0400

    Fixed the issue in the router!!
    Fixed the issue in the router.php where the image in err404.php would not be displayed when rerouting occurs.

---
+++ Views/Templates/err404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Error Resource Not Found</title>
+</head>
+
+<body>
+    <header>
+        <h1>Hello.. Is It me ? Your looking for ...</h1>
+    </header>
+    <main>
+        <img src=<?php echo "Views/Templates/images/Hello_Lionel.jpg" ?> alt="Lionel Richie">
+    </main>
+</body>
+
+</html>
