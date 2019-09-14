d852510845d5557fa75c7f26b654343c4cf75cdf
Jaleel Blue <jaleel.blue97@gmail.com>
2019-09-14 01:14:30 -0400

    Added to paths to the router
    Added the base routes to the router.

--- router.php
+++ router.php
@@ -2,6 +2,16 @@
 <?php 
     $request = $_SERVER["REQUEST_URI"];
 
+    
+    
     switch($request){
+        case "/":
+            require __DIR__ . "/index.html";
+            break;
+        default:
+            require __DIR__ . "/views/404.php";
+            break;
     }
+
+
 ?>
