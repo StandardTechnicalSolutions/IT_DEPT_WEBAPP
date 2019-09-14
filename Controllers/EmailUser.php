a4ae5582685b5cff2b7fe50a8de3b3ecaf83733a
Jaleel Blue <jaleel.blue97@gmail.com>
2019-09-14 00:48:07 -0400

    Completed Email Functionality
    finished the email functionality

--- Controllers/EmailUser.php
+++ Controllers/EmailUser.php
@@ -11,5 +11,7 @@
     $message .= "\n Thank you for ordering " . $orderQuantity . " many scantrons.";
     $message .= "\n Your order will be available in 5 - 7 business days.";
 
-    $fromSender = "";
+    $fromSender = "gkovalick@bloomu.edu";
+
+    mail($userEmail, $subject, $message, $fromSender);
 ?>
