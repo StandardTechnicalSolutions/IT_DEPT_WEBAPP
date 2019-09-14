1f2b82424f96f839856d7c9a794e51fe96f0e817
Jaleel Blue <jaleel.blue97@gmail.com>
2019-09-13 22:02:20 -0400

    Basic Project Configuration
    Set Up the basic MVC file structure

---
+++ Views/Templates/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>BU IT-DEPT</title>
+        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <header>
+            <h1>Select Your Viewing Preference</h1>
+        </header>
+        <main>
+            <div id="container">
+                <button id="desktop"><a href="#"></a></button>
+                <button id="desktop"><a href="#"></a></button>
+            </div>
+        </main>
+        <footer>
+            <div id="footer">
+            </div>
+        </footer>
+        <script src=""></script>
+    </body>
+</html>
