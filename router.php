fbb3c4312078dc68727889d3ff275e5cb2da53b9
Jaleel Blue <jaleel.blue97@gmail.com>
2019-09-14 20:17:42 -0400

    Updated router
    Added new routes to router.php

--- router.php
+++ router.php
@@ -8,6 +8,9 @@
         case "/":
             require __DIR__ . "/index.html";
             break;
+        case "/equipment":
+            require "Controllers/equipmentContent.php";
+            require "Views/Templates/desktop-template.html";
         default:
             require __DIR__ . "/Views/Templates/err404.php";
             break;
